686496bc66b1d5f488e0cd091c87675e70985073
Antoine <nonown745@gmail.com>
2025-06-19 17:44:42 +0200

    flexbox Home done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,13 @@
-
+import '../styles/Card.scss'
 
 
 export default function Card({title, cover, description}) {
     return (
-        <div className="card">
-        <img src={cover} alt={description} className="card__img" />
-        <h2 className={title}>{title}</h2>
+        <div className="card" style={{ backgroundImage: `url(${cover})` }}
+      aria-label={description}>
+        <div className="card__overlay">
+        <h2 className="card__title" alt={title}>{title}</h2>
+        </div>
         </div>
     )
 }
