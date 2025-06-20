d788625abd61bdbf5f2343361eb9d1519142b324
Antoine <nonown745@gmail.com>
2025-06-20 14:40:14 +0200

    add page 404

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
 import Home from './pages/Home'
 import Footer from './components/Footer'
+import NotFound from './components/notFound'
 import './styles/App.scss'
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
 
       <Routes>
        <Route path="/" element={<Home/>} />
-
+       <Route path="*" element={<NotFound />} />
       </Routes>
 
       <Footer />
