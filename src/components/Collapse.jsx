d15dce08de189207f53a23fb5238c4905b93d1d3
Antoine Fabre <nonown745@gmail.com>
2025-07-11 14:25:15 +0200

    format document

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -3,24 +3,24 @@ import '../styles/Collapse.scss'
 import downArrow from '../assets/Vector_down.svg'
 
 
-export default function Collapse({title, content}) { 
-const [isOpen, set] = useState(false)
-const openAndClose = () => {set(!isOpen)}
-return (
-<div className="collapse">
-<div className="collapse__header" onClick={openAndClose}>
+export default function Collapse({ title, content }) {
+  const [isOpen, set] = useState(false)
+  const openAndClose = () => { set(!isOpen) }
+  return (
+    <div className="collapse">
+      <div className="collapse__header" onClick={openAndClose}>
 
-<h2 className="collapse__title">{title}</h2>
-<span className="collapse__span"><img src={downArrow} className={isOpen ? 'rotate': 'rotate__up'} /></span>
-</div>
-<div className={`collapse__container ${isOpen ? 'open' : 'close'}`}>
-  <p className="collapse__content">{content}</p>
-</div>
+        <h2 className="collapse__title">{title}</h2>
+        <span className="collapse__span"><img src={downArrow} className={isOpen ? 'rotate' : 'rotate__up'} /></span>
+      </div>
+      <div className={`collapse__container ${isOpen ? 'open' : 'close'}`}>
+        <p className="collapse__content">{content}</p>
+      </div>
 
-</div>
-
-
+    </div>
 
 
-)
+
+
+  )
 }
